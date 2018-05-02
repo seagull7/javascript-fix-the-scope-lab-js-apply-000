16c80bade0c19475939452dfc9da2e30dc94e7cc
seagull7 <samsiegel527@gmail.com>
2018-05-02 20:52:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function funkyFunction() {
 
     return "FUNKY!"
   }
-}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
